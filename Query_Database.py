ce9cc17474ec6f4e0bae222f3db5cf742ecaa01b
Matt <mattheer@usc.edu>
2020-03-29 13:18:41 -0700

    Initial commit, demo of the query only

---
+++ Query_Database.py
@@ -0,0 +1,56 @@
+# Created by Matthew Rose
+# AME 505 Project
+#
+# This file is intended to query the birdstrike
+# It will take a while to load the first time, but the second run happens much faster
+
+# This library is used to query Access database
+import pyodbc
+# This library is used to find the path of your database
+import os.path
+
+# ## Variables that may need to be changed #############################################################################
+# This is the name of the database that needs to be accessed by python
+# The database is .accdb
+driver = '{Microsoft Access Driver (*.mdb, *.accdb))}'
+
+# Location of your birdstrike database
+filepath = os.path.dirname(__file__) + "/Database/wildlife.accdb"
+print(filepath)
+
+# The table name is name of the table inside the Access database
+table_name = 'STRIKE_REPORTS'
+
+# The query is actually SQL code that is being run by python
+# In this query I am creating a string that is asking for
+# everything in the database with the database name STRIKE_REPORTS
+query = "SELECT * FROM {}".format(table_name)
+
+# ######################################################################################################################
+
+# ## Functions that are being run ######################################################################################
+
+# Access the library and call this function dataSources
+# From GitHub Returns a dictionary mapping available DSNs to their descriptions.
+# On Windows, these will be the ones defined in the ODBC Data Source Administrator.
+myDataSources = pyodbc.dataSources()
+print(myDataSources)
+
+# Passing the type of database that we want to access (This may need a driver if your dataSources dictionary is empty)
+access_driver = myDataSources['MS Access Database']
+
+# This is using the library and calling the connect function to access the database
+# The two imports are the driver (type of file) of the database and where the database is located
+connection = pyodbc.connect(driver=access_driver, dbq=filepath)
+
+# Cursor is an object that is created once the connection is successful
+# From documentation on SQLite3 A Cursor instance has attributes and methods
+cursor = connection.cursor()
+
+# This is a method for the cursor object, you can execute a query which injects the SQL code using python
+cursor.execute(query)
+
+# This is an example of what comes out when accessing the database
+one_row = cursor.fetchone()
+# This allows you to see what the first row in the database looks like
+print(one_row)
