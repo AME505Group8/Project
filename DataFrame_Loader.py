658ac2daeab83c2b5acaf5f7bdd4bd7fac6bfadf
Matt <mattheer@usc.edu>
2020-04-12 09:51:00 -0700

    Update to load data
    This allows users to load data from files instead of processing it each run

--- DataFrame_Loader.py
+++ DataFrame_Loader.py
@@ -5,9 +5,63 @@
 # This file is intended to load the existing dataframe called BIRD_STRIKE.pkl
 # It should load quickly
 
+# This library is used to query Access database
+import pyodbc
+# This library is used to find the path of your database
+import os.path
 # Library for dataframe
 import pandas
+from pandas import DataFrame
 import numpy
+import pickle
+
+
+def query_database(filepath, query, cursor_dict):
+
+    # Access the library and call this function dataSources
+    # From GitHub Returns a dictionary mapping available DSNs to their descriptions.
+    # On Windows, these will be the ones defined in the ODBC Data Source Administrator.
+    myDataSources = pyodbc.dataSources()
+    print(myDataSources)
+
+    # Passing the type of database that we want to access (This may need a driver if your dataSources dictionary is empty)
+    access_driver = myDataSources['MS Access Database']
+
+    # This is using the library and calling the connect function to access the database
+    # The two imports are the driver (type of file) of the database and where the database is located
+    connection = pyodbc.connect(driver=access_driver, dbq=filepath)
+
+    # Cursor is an object that is created once the connection is successful
+    # From documentation on SQLite3 A Cursor instance has attributes and methods
+    cursor = connection.cursor()
+
+    # This is a method for the cursor object, you can execute a query which injects the SQL code using python
+    cursor.execute(query)
+
+    # This pulls off the names of from the header
+    columns = [column[0] for column in cursor.description]
+
+    # Show the output of the columns to make sure it looks correct
+    print(columns)
+
+    # Message to user because the conversion is slow
+    print('Starting Conversion')
+
+    # This code turns the columns into the dictionary name, row is the data
+    for row in cursor.fetchall():
+        cursor_dict.append(dict(zip(columns, row)))
+
+    # Message to user because this is a slow process
+    print('Setting dataframe')
+
+    # Load the cursor object as a DataFrame object
+    df = DataFrame(cursor_dict)
+
+    # Check that the DataFrame loads correctly
+    print(df)
+
+    # Save the dataframe as a pickle file (compresses the database to load much faster)
+    df.to_pickle("BIRD_STRIKE.pkl")
 
 
 def dataframe_loader(filename):
@@ -22,6 +76,19 @@ def dataframe_loader(filename):
     print(df)
     return df
 
+
+def save_dictionary(obj, name):
+    print('Saving dictionary')
+    with open(name, 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_dictionary(name):
+    print('Loading dictionary')
+    with open(name, 'rb') as f:
+        return pickle.load(f)
+
+
 def dataframe_saver(dataframe, filename):
     # This is a quick way to save off a new file so that changing the data doesn't permanently change the original data
 
@@ -35,10 +102,12 @@ def dataframe_saver(dataframe, filename):
     # Message to user
     print('Data saved!')
 
+
 def unique(sequence):
     # Code from Martin Broadhurst (http://www.martinbroadhurst.com/) Date accessed 4/5/2020
     seen = set()
     return [x for x in sequence if not (x in seen or seen.add(x))]
+
 
 def handle_non_numerical_data(df):
     # This function turns all the non numerical data into numbers for the machine learning code to use
@@ -48,6 +117,9 @@ def handle_non_numerical_data(df):
 
     # Columns grab the header text from the dataframe
     columns = df.columns.values
+
+    # Empty dictionary to initialize variable
+    store_dict = {}
 
     # This for loop goes through each column, one by one
     for column in columns:
@@ -79,6 +151,7 @@ def handle_non_numerical_data(df):
 
             # This maps the list convert_to_int to the position in the column and writes over the current column
             df[column] = list(map(convert_to_int, df[column]))
+            
         # This else statement was added because datastamps need special treatment in the function and will not work
         # with the strategy above
         elif pandas.core.dtypes.common.is_datetime_or_timedelta_dtype(df[column]):
@@ -90,5 +163,7 @@ def handle_non_numerical_data(df):
                 timestamp.append(df[column][y].timestamp())
             # places the list into the column since each timestamp needs to be recorded
             df[column] = timestamp
+
+        store_dict[column] = text_digit_vals
     print(df)
-    return df
+    return df, store_dict
--- Project_Main.py
+++ Project_Main.py
@@ -3,7 +3,9 @@
 #
 # This is the main file to call multiple functions and store variable names
 
-from DataFrame_Loader import dataframe_loader, handle_non_numerical_data, unique
+import os.path
+from DataFrame_Loader import dataframe_loader, handle_non_numerical_data, dataframe_saver, query_database, \
+    save_dictionary, load_dictionary
 
 # ## Variables that may need to be changed #############################################################################
 
@@ -11,10 +13,7 @@ from DataFrame_Loader import dataframe_loader, handle_non_numerical_data, unique
 # unless the BIRD_STRIKE.pkl file is corrupted on the Google Drive
 fresh_load = False
 
-# The filename where BIRD_STRIKE dataframe is saved. This file has not been preconditioned. If you need the
-# preconditoned file (like running machine learning), recommend using a different file
-raw_data_filename = "BIRD_STRIKE.pkl"
-
+# Load variables only for fresh_load
 if fresh_load:
     # This is the name of the database that needs to be accessed by python
     # The database is .accdb
@@ -34,56 +33,141 @@ if fresh_load:
     # This is needed to initialize the dictionary for the dataframe
     cursor_dict = []
 
+# This is only if you need to revert back to the dataframe before it was preconditioned
+original_dataframe = False
+
+# Load variables only for original_dataframe
+if original_dataframe:
+    # The filename where BIRD_STRIKE dataframe is saved. This file has not been preconditioned. If you need the
+    # preconditoned file (like running machine learning), recommend using a different file
+    original_data_filename = "BIRD_STRIKE.pkl"
+
+    # This is if you want to convert all the non number part of the database into numbers only
+    condition_data = False
+
+    # This is if you want to save off the data after it is preconditioned. WARNING, it will overwrite any data saved as
+    # conditioned_data_filename
+    save_conditioned = False
+
+# This is if you want to only load the dataframe after it has been preconditioned
+conditioned_dataframe = True
+
+# Load data only for conditioned_dataframe
+if conditioned_dataframe:
+    # The filename where the preconditioned data is saved after the preconditioning is complete. Recommend using for
+    # machine learning
+    conditioned_data_filename = "BIRD_STRIKE_NUM_ONLY.pkl"
+
+    # The filename where BIRD_STRIKE dictionary is saved. This file is for conditioned data
+    dictionary_filename = "BIRD_STRIKE_DICTIONARY.pkl"
+
 # ######################################################################################################################
 
 # ## Function Calls ####################################################################################################
 
-# This is a function call in order to pull the raw data
-df = dataframe_loader(raw_data_filename)
+# This function is to load the database for the first time
+if fresh_load:
+    query_database(filepath, query, cursor_dict)
 
-# Correct output looks like this ###########################
-#         INDEX_NR INCIDENT_DATE  ...    LUPDATE  TRANSFER #
-# 0         613189    1993-01-24  ... 2012-07-02     False #
-# 1         613814    1992-05-01  ... 2012-06-11     False #
-# 2         614017    1991-08-16  ... 2016-04-08     False #
-# 3         614185    1991-12-18  ... 2012-06-11     False #
-# 4         614652    1992-02-07  ... 2015-07-31     False #
-# ...          ...           ...  ...        ...       ... #
-# 233667    934325    2019-08-20  ... 2020-02-07     False #
-# 233668    922514    2019-08-08  ... 2020-01-13     False #
-# 233669    909147    2019-06-20  ... 2019-12-04     False #
-# 233670    921636    2019-08-06  ... 2020-01-02     False #
-# 233671    922174    2019-08-07  ... 2020-01-08     False #
-# ##########################################################
+# This is a function call in order to pull the original data
+elif original_dataframe:
+    df = dataframe_loader(original_data_filename)
 
-# Recommended so that the first index is the first date in the database, INDEX_NR is not in chronological order
-df = df.sort_values(by=['INCIDENT_DATE'])
-# Check that it worked
-print(df)
-# Allows list to have no blanks
-df['TIME_OF_DAY'] = df['TIME_OF_DAY'].fillna('')
-# Records all the strings
-column_contents = df['TIME_OF_DAY'].values.tolist()
-# Check that the unique function worked thanks to www.martinbroadhurst.com
-unique_elements = unique(column_contents)
-# Check that it worked
-print(unique_elements)
-# Should see the following ['Night', '', 'Dusk', 'Day', 'Dawn']
+    # Correct output looks like this ###########################
+    #         INDEX_NR INCIDENT_DATE  ...    LUPDATE  TRANSFER #
+    # 0         613189    1993-01-24  ... 2012-07-02     False #
+    # 1         613814    1992-05-01  ... 2012-06-11     False #
+    # 2         614017    1991-08-16  ... 2016-04-08     False #
+    # 3         614185    1991-12-18  ... 2012-06-11     False #
+    # 4         614652    1992-02-07  ... 2015-07-31     False #
+    # ...          ...           ...  ...        ...       ... #
+    # 233667    934325    2019-08-20  ... 2020-02-07     False #
+    # 233668    922514    2019-08-08  ... 2020-01-13     False #
+    # 233669    909147    2019-06-20  ... 2019-12-04     False #
+    # 233670    921636    2019-08-06  ... 2020-01-02     False #
+    # 233671    922174    2019-08-07  ... 2020-01-08     False #
+    # ##########################################################
 
-# This converts the dataframe into a numbers only format
-df_numbers = handle_non_numerical_data(df)
+    # This replaces blank space with and empty string (helps with machine learning)
+    df = df.fillna('')
 
-# Correct output looks like this #######################################################
-#         INDEX_NR  INCIDENT_DATE  INCIDENT_MONTH  ...  PERSON       LUPDATE  TRANSFER #
-# 5124      841708   7.278336e+08               8  ...       0  1.341187e+09         0 #
-# 11603     608206   7.046784e+08               3  ...       1  1.339373e+09         0 #
-# 116097    608216   6.823008e+08               4  ...       2  1.460074e+09         0 #
-# 116090    608181   6.930144e+08               4  ...       0  1.339373e+09         0 #
-# 116091    608182   6.974208e+08               4  ...       0  1.438301e+09         0 #
-# ...          ...            ...             ...  ...     ...           ...       ... #
-# 230865    945904   1.566259e+09              10  ...       3  1.581034e+09         0 #
-# 59905     934903   1.565222e+09              10  ...       5  1.578874e+09         0 #
-# 47149     934905   1.560989e+09              10  ...       1  1.575418e+09         0 #
-# 232542    909294   1.565050e+09              10  ...       3  1.577923e+09         0 #
-# 9833      921068   1.565136e+09              10  ...       5  1.578442e+09           #
-# ######################################################################################
+    if condition_data:
+        # This converts the dataframe into a numbers only format
+        df_numbers, text_values = handle_non_numerical_data(df)
+
+        # Correct output looks like this #######################################################
+        #         INDEX_NR  INCIDENT_DATE  INCIDENT_MONTH  ...  PERSON       LUPDATE  TRANSFER #
+        # 0         613189   7.278336e+08               1  ...       5  1.341187e+09         0 #
+        # 1         613814   7.046784e+08               5  ...       2  1.339373e+09         0 #
+        # 2         614017   6.823008e+08               8  ...       4  1.460074e+09         0 #
+        # 3         614185   6.930144e+08              12  ...       2  1.339373e+09         0 #
+        # 4         614652   6.974208e+08               2  ...       4  1.438301e+09         0 #
+        # ...          ...            ...             ...  ...     ...           ...       ... #
+        # 233667    934325   1.566259e+09               8  ...       1  1.581034e+09         0 #
+        # 233668    922514   1.565222e+09               8  ...       1  1.578874e+09         0 #
+        # 233669    909147   1.560989e+09               6  ...       1  1.575418e+09         0 #
+        # 233670    921636   1.565050e+09               8  ...       2  1.577923e+09         0 #
+        # 233671    922174   1.565136e+09               8  ...       5  1.578442e+09         0 #
+        # ######################################################################################
+
+        print(text_values['STATE'])
+
+        # ################################################################################################################
+        # {'HI': 0, 'BC': 1, 'CA': 2, 'NV': 3, 'PI': 4, '': 5, 'ON': 6, 'PA': 7, 'LA': 8, 'NY': 9, 'MD': 10, 'SC': 11,   #
+        #  'TN': 12, 'OH': 13, 'NC': 14, 'IL': 15, 'TX': 16, 'WV': 17, 'GA': 18, 'NJ': 19, 'IA': 20, 'OK': 21, 'WI': 22, #
+        #  'ME': 23, 'IN': 24, 'FL': 25, 'MS': 26, 'VA': 27, 'CT': 28, 'AL': 29, 'KY': 30, 'DC': 31, 'CO': 32, 'MI': 33, #
+        #  'AR': 34, 'NH': 35, 'KS': 36, 'MO': 37, 'MN': 38, 'NE': 39, 'MA': 40, 'AB': 41, 'NM': 42, 'AZ': 43, 'UT': 44, #
+        #  'RI': 45, 'ID': 46, 'OR': 47, 'WA': 48, 'FN': 49, 'PR': 50, 'VT': 51, 'AK': 52, 'QC': 53, 'MT': 54, 'ND': 55, #
+        #  'WY': 56, 'SD': 57, 'VI': 58, 'DE': 59, 'NL': 60, 'MB': 61, 'NS': 62, 'SK': 63}                               #
+        # ################################################################################################################
+
+        # This lets you save the df_numbers dataframe to a file, to load the next time skipping the
+        # processing part of the script
+        if save_conditioned:
+            dataframe_saver(df_numbers, conditioned_data_filename)
+            # This lets you save the dictionary that goes along with the df_numbers dataframe
+            save_dictionary(text_values, dictionary_filename)
+
+# This lets you load only the dataframe that has been conditioned without having to process the
+# data on each run
+elif conditioned_dataframe:
+    # This loads the saved conditioned file
+    df = dataframe_loader(conditioned_data_filename)
+    # This loads the dictionary file that goes with the conditioned data
+    text_names = load_dictionary(dictionary_filename)
+
+    # Correct output looks like this #######################################################
+    #         INDEX_NR  INCIDENT_DATE  INCIDENT_MONTH  ...  PERSON       LUPDATE  TRANSFER #
+    # 0         613189   7.278336e+08               1  ...       5  1.341187e+09         0 #
+    # 1         613814   7.046784e+08               5  ...       2  1.339373e+09         0 #
+    # 2         614017   6.823008e+08               8  ...       4  1.460074e+09         0 #
+    # 3         614185   6.930144e+08              12  ...       2  1.339373e+09         0 #
+    # 4         614652   6.974208e+08               2  ...       4  1.438301e+09         0 #
+    # ...          ...            ...             ...  ...     ...           ...       ... #
+    # 233667    934325   1.566259e+09               8  ...       1  1.581034e+09         0 #
+    # 233668    922514   1.565222e+09               8  ...       1  1.578874e+09         0 #
+    # 233669    909147   1.560989e+09               6  ...       1  1.575418e+09         0 #
+    # 233670    921636   1.565050e+09               8  ...       2  1.577923e+09         0 #
+    # 233671    922174   1.565136e+09               8  ...       5  1.578442e+09         0 #
+    # ######################################################################################
+
+    print(df.columns.values)
+    print(text_names['STATE'])
+
+    # Correct output looks like this #################################################################################
+    # {'HI': 0, 'BC': 1, 'CA': 2, 'NV': 3, 'PI': 4, '': 5, 'ON': 6, 'PA': 7, 'LA': 8, 'NY': 9, 'MD': 10, 'SC': 11,   #
+    #  'TN': 12, 'OH': 13, 'NC': 14, 'IL': 15, 'TX': 16, 'WV': 17, 'GA': 18, 'NJ': 19, 'IA': 20, 'OK': 21, 'WI': 22, #
+    #  'ME': 23, 'IN': 24, 'FL': 25, 'MS': 26, 'VA': 27, 'CT': 28, 'AL': 29, 'KY': 30, 'DC': 31, 'CO': 32, 'MI': 33, #
+    #  'AR': 34, 'NH': 35, 'KS': 36, 'MO': 37, 'MN': 38, 'NE': 39, 'MA': 40, 'AB': 41, 'NM': 42, 'AZ': 43, 'UT': 44, #
+    #  'RI': 45, 'ID': 46, 'OR': 47, 'WA': 48, 'FN': 49, 'PR': 50, 'VT': 51, 'AK': 52, 'QC': 53, 'MT': 54, 'ND': 55, #
+    #  'WY': 56, 'SD': 57, 'VI': 58, 'DE': 59, 'NL': 60, 'MB': 61, 'NS': 62, 'SK': 63}                               #
+    # ################################################################################################################
+
+    print(text_names['TIME_OF_DAY'])
+
+    # Correct output looks like this ######################
+    # {'Day': 0, 'Dusk': 1, 'Night': 2, 'Dawn': 3, '': 4} #
+    # #####################################################
+
+else:
+    print('Check your loading variables, both are currently False')
